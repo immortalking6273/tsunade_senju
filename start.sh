ae99ace5e9d5243cdb6886a68006a3e35d9baeb3
immortalking6273 <142087389+immortalking6273@users.noreply.github.com>
2023-08-20 15:02:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/immortalking6273/tsunade_senju/tree/main
+  git clone https://github.com/immortalking6273/tsunade_senju
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-The-File-Donor
